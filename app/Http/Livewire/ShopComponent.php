52ad471cec4a54e17e0d4a13ae3ee93c3d81de88
Mohamed-Nour <87799327+Mohamed-Nour-Abshir@users.noreply.github.com>
2022-03-17 02:02:26 +0600

    added sort by price low to high high to low and sort by pages functions

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -9,6 +9,14 @@ use Cart;
 
 class ShopComponent extends Component
 {
+    public $sorting;
+    public $pageSize;
+
+    public function mount(){
+        $this->sorting ="default";
+        $this->pageSize = 12;
+    }
+
     public function store($product_id, $product_name, $product_price){
      Cart::add($product_id,$product_name, 1, $product_price)->associate('App\Models\Product');
      session()->flash('success_message', 'Item added to the Cart');
@@ -17,7 +25,19 @@ class ShopComponent extends Component
     // use WithPagination;
     public function render()
     {
-        $products = product::paginate(12);
+        if($this->sorting == 'date'){
+           $products = product::orderBy('created_at','DESC')->paginate($this->pageSize);
+        }
+        else if($this->sorting == 'price'){
+            $products = product::orderBy('regular_price','ASC')->paginate($this->pageSize);
+         }
+         else if($this->sorting == 'price-desc'){
+            $products = product::orderBy('regular_price','DESC')->paginate($this->pageSize);
+         }
+         else{
+            $products = product::paginate($this->pageSize);
+         }
+
         return view('livewire.shop-component',['products'=>$products])->layout('layouts.home');
     }
 }

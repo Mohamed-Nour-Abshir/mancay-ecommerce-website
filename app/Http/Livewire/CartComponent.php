29ded755c1611ddcb6728db2a24debcb4b7e71db
Mohamed-Nour <87799327+Mohamed-Nour-Abshir@users.noreply.github.com>
2022-03-17 01:36:22 +0600

    added delete item from cart and delete all items from cart functions

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -7,15 +7,27 @@ use Cart;
 
 class CartComponent extends Component
 {
+    // increasing cart quantity
     public function increaseQuantity($rowId){
         $product = Cart::get($rowId);
         $qty = $product->qty + 1;
         Cart::update($rowId,$qty);
     }
+      // decreasing cart quantity
     public function decreaseQuantity($rowId){
         $product = Cart::get($rowId);
         $qty = $product->qty - 1;
         Cart::update($rowId,$qty);
+    }
+    //deleting item form the cart
+    public function destroy($rowId){
+        Cart::remove($rowId);
+        Session()->flash('success_message', 'Item has been removed');
+    }
+    // deleting all items form cart
+    public function destroyAll(){
+        Cart::destroy();
+        Session()->flash('success_message', 'All Items has been removed');
     }
     public function render()
     {

a3caa1fe599b99d923bd8b4e0f14f10251196222
Mohamed-Nour <87799327+Mohamed-Nour-Abshir@users.noreply.github.com>
2022-04-20 22:22:44 +0600

    added admin-dashboard show-details

--- app/Http/Livewire/Admin/AdminComponent.php
+++ app/Http/Livewire/Admin/AdminComponent.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Order;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class AdminComponent extends Component
 {
     public function render()
     {
-        return view('livewire.admin.admin-component')->layout('layouts.home');
+        $ordders = Order::orderBy('created_at','DESC')->get()->take(10);
+        $totalSales = Order::where('status','delivered')->count();
+        $totalRevenue = Order::where('status','delivered')->sum('total');
+        $todaySales = Order::where('status','delivered')->whereDate('created_at',Carbon::today())->count();
+        $todayRevenue = Order::where('status','delivered')->whereDate('created_at',Carbon::today())->sum('total');
+        return view('livewire.admin.admin-component',['orders'=>$ordders,'totalSales'=>$totalSales,'totalRevenue'=>$totalRevenue,'todaySales'=>$todaySales,'todayRevenue'=>$todayRevenue])->layout('layouts.home');
     }
 }
